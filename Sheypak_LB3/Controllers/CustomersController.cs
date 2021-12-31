14f4c161a7c74d2a8e1ca6a2338605adc12b0728
181701_Evgenii_Sheypak <91116024+181701cSheypakEM@users.noreply.github.com>
2021-12-31 15:18:31 +0300

    Add files via upload

---
+++ Sheypak_LB3/Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using Sheypak_LB3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Sheypak_LB3.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Add(int productId)
+        {
+            return View(StaticDB.Products.FirstOrDefault(x=>x.Id == productId));
+        }
+    }
+}
